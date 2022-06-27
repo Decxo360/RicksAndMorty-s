42ca6024a49942cf309530cd10e9ba11f332e91f
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-06-26 20:22:02 -0400

    configuracion de mi store

--- src/store/store.js
+++ src/store/store.js
@@ -1,7 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit'
+import { CounterSlice } from './slices/counter/CounterSlice'
+import {ricksAndMortysApi} from './api/RicksAndMortys'
 
-const store = configureStore({
-  
+export const store = configureStore({
+reducer:{
+    counter: CounterSlice.reducer,
+    [ricksAndMortysApi.reducerPath]: ricksAndMortysApi.reducer,
+},
+middleware: (getDefaultMiddleware)=>getDefaultMiddleware().concat(ricksAndMortysApi.middleware)
 })
-
-export default store
