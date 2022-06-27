b0a374aa580714182d7ac08c0b8bbc837e21cfd9
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-06-26 20:21:10 -0400

    implementacion de estilos y counter reducer

--- src/App.jsx
+++ src/App.jsx
@@ -1,45 +1,31 @@
-import { useState } from 'react'
-import logo from './logo.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+import { useDispatch, useSelector } from 'react-redux'
+import { CharacterContent } from './components/CharacterContent'
+import { increment,decrement,reset } from './store/slices/counter/CounterSlice'
+
+export const App =()=> {
+
+  const counter =useSelector(state=>state.counter)
+  const dispatch = useDispatch();
+
+
+  console.log(counter);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.jsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+    <div>
+        <img src='./src/logo.png' className='img-fluid' alt='algoctm' style={{width:'25%',marginLeft:'39%'}}/>
+        <div className='d-flex justify-content-between' style={{marginTop:'20px'}}>
+          <button className='btn btn-outline-primary' disabled={counter.page === 1 ? true : false} onClick={()=>dispatch(decrement())}>Anterior</button>
+          <button className='btn btn-outline-primary' onClick={()=>dispatch(reset())}>Resetear</button>
+          <button className='btn btn-outline-primary' onClick={()=>dispatch(increment())} style={{right:'0px'}}>Siguiente</button>
+        </div>
+
+        <CharacterContent/>
+
+
     </div>
   )
 }
 
-export default App
+
