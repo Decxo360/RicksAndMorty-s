36297e1500125699475114bd3faa9cd17412f06f
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-06-27 00:10:12 -0400

    implementacion de hook, y datos de la api

--- src/App.jsx
+++ src/App.jsx
@@ -3,25 +3,55 @@ import './App.css'
 
 import { useDispatch, useSelector } from 'react-redux'
 import { CharacterContent } from './components/CharacterContent'
-import { increment,decrement,reset } from './store/slices/counter/CounterSlice'
+import { increment, decrement, reset } from './store/slices/counter/CounterSlice'
+import { useGetCharactersQuery, useGetRicksAndMortysQuery } from './store/api/RicksAndMortys'
+import { useState } from 'react'
 
-export const App =()=> {
+export const App = () => {
 
-  const counter =useSelector(state=>state.counter)
+  const counter = useSelector(state => state.counter)
   const dispatch = useDispatch();
 
+  const [character,setCharacter] = useState(1)
 
-  console.log(counter);
+  const { data = [], isLoading } = useGetRicksAndMortysQuery(character)
+  const { results } = data
+
+
+  const Next=()=>{
+    setCharacter(character+1)
+  }
+  const reset=()=>{
+    setCharacter(1)
+  }
+  const Previus=()=>{
+    setCharacter(character-1)
+  }
+
   return (
     <div>
-        <img src='./src/logo.png' className='img-fluid' alt='algoctm' style={{width:'25%',marginLeft:'39%'}}/>
-        <div className='d-flex justify-content-between' style={{marginTop:'20px'}}>
-          <button className='btn btn-outline-primary' disabled={counter.page === 1 ? true : false} onClick={()=>dispatch(decrement())}>Anterior</button>
-          <button className='btn btn-outline-primary' onClick={()=>dispatch(reset())}>Resetear</button>
-          <button className='btn btn-outline-primary' onClick={()=>dispatch(increment())} style={{right:'0px'}}>Siguiente</button>
-        </div>
-
-        <CharacterContent/>
+      <img src='./src/logo.png' className='img-fluid' alt='algoctm' style={{ width: '25%', marginLeft: '39%' }} />
+      <div className='d-flex justify-content-between' style={{ marginTop: '20px' }}>
+        <button className='btn btn-outline-primary' disabled={character === 1 ? true : false} onClick={Previus}>Anterior</button>
+        <button className='btn btn-outline-primary' onClick={reset}>Resetear</button>
+        <button className='btn btn-outline-primary' onClick={Next} style={{ right: '0px' }}>Siguiente</button>
+      </div>
+      <div className='d-flex flex-wrap'>
+        {!isLoading 
+        ? 
+        results?.map(result => (
+          <CharacterContent
+            key={result.id}
+            img={result.image}
+            name={result.name}
+            species={result.species}
+            gender={result.gender}
+            status={result.status}
+          />
+        ))
+        : 
+        <>Cargando.....</>}
+      </div>
 
 
     </div>
