dc9bb7eb3ca8b41d42bc2c1d0138549efb9967b9
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-06-27 17:25:29 -0400

    implementacion de la funcionalidad

--- src/components/SearchComponent.jsx
+++ src/components/SearchComponent.jsx
@@ -1,30 +1,69 @@
+import { useState } from "react";
+import { useForm } from "../hooks/useForm"
+import { useGetCharacterByNameQuery } from "../store/api/RicksAndMortys";
+import { CharacterContent } from "./CharacterContent";
 
 
 export const SearchComponent = () => {
+    const [search, setSearch] = useState('')
+    const [{ busqueda }, handleInputChange, reset] = useForm({ busqueda: '' })
+
+    const { data = [], isLoading } = useGetCharacterByNameQuery(search)
+    console.log(data);
+    const onAddSearch = (e) => {
+        e.preventDefault();
+        setSearch(busqueda);
+    }
+
     return (
-        <div className="d-flex flex-row justify-content-center mt-3">
+        <div>
+            <div className="d-flex flex-row justify-content-center mt-3">
                 <form>
                     <input
                         type="text"
                         placeholder="  ej: Rick Sanchez"
+                        name='busqueda'
+                        value={busqueda}
+                        onChange={handleInputChange}
                         style={{
                             height: '40px',
                             borderRadius: '5px 0px 0px 5px',
                             border: '1px solid white',
-                            backgroundColor:'#000000',
-                            color:"white",
-                            borderRight:'none'
+                            backgroundColor: '#000000',
+                            color: "white",
+                            borderRight: 'none'
                         }} />
                 </form>
-                <button  type="button" style={{
+                <button type="button" style={{
                     borderRadius: '0px 5px 5px 0px',
                     border: '1px solid white',
-                    borderLeft:'none',
+                    borderLeft: 'none',
                     height: '40px',
                     marginTop: '0px',
-                    backgroundColor:'#000000',
-                    color:'white'
-                }}>Buscar</button>
+                    backgroundColor: '#000000',
+                    color: 'white'
+                }}
+                    onClick={onAddSearch}>Buscar</button>
+            </div>
+            {isLoading
+                ? <h1 className="d-flex justify-content-center" style={{color:'white'}}>Cargando...</h1>
+                :
+                <div>
+                    <h3 className="d-flex justify-content-center" style={{color:'white'}}>{search !== '' ? `Su busqueda fue: ${busqueda}` : '' }</h3>
+                    <div className={`d-flex flex-wrap ${data.results.length < 3 ? 'justify-content-center' : '' }`}>
+                        {data.results.map(result => (
+                            <CharacterContent
+                                key={result.id}
+                                img={result.image}
+                                name={result.name}
+                                species={result.species}
+                                gender={result.gender}
+                                status={result.status}
+                            />))}
+                    </div>
+                </div>
+
+            }
         </div>
     )
 }
