b2dd8826be5b9642f2bec0be1ca6448449efab7f
Diego Lundstedt <diegolundstedt99@gmail.com>
2022-06-26 20:21:48 -0400

    contenedor de la data de la api de rick and morty

---
+++ src/components/CharacterContent.jsx
@@ -0,0 +1,19 @@
+
+
+export const CharacterContent = () => {
+  return (
+    <div style={{marginTop:'20px',border:'1px solid #424242',width:'200px',borderRadius:'5px', marginLeft:'20px' }} >
+        <div style={{marginLeft:'22px', marginTop:'10px'}}>
+            <img src="https://s3-prod.adage.com/s3fs-public/styles/1200x630/public/20210614_rick_morty_3x2.jpg" 
+                alt="" style={{width:'150px',height:'150px',borderRadius:'75px'}} />
+        </div>
+        <div style={{marginTop:'20px'}}>
+            <h5 style={{color:'wheat',marginLeft:'39%'}}>algo</h5>
+            <h5 style={{color:'wheat',marginLeft:'39%'}}>algo</h5>
+            <h5 style={{color:'wheat',marginLeft:'39%'}}>algo</h5>
+            <h5 style={{color:'wheat',marginLeft:'39%'}}>algo</h5>
+        </div>
+
+    </div>
+  )
+}
