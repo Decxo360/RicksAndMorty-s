1d1cb1d2286a2d1c61231cdd147839366350c16b
Decxo360 <diegolundstedt99@gmail.com>
2023-08-15 21:02:43 -0400

    fix: icono cambiado

--- src/App.jsx
+++ src/App.jsx
@@ -24,8 +24,8 @@ export const App = () => {
 
   return (
     <div>
-      <img src='./src/logo.png' className='img-fluid' alt='algoctm' style={{ width: '25%', marginLeft: '39%' }} />
-      <div className='d-flex justify-content-between' style={{ marginTop: '20px' }}>
+      <img src='https://res.cloudinary.com/drp7cwb8s/image/upload/v1692131103/kisspng-rick-sanchez-youtube-morty-smith-rick-and-morty-5af4f912cb4576.6530769515260039868326_ijm2yk.png' className='img-fluid' alt='algoctm' style={{ width: '25%', marginLeft: '39%' }} />
+      <div className='d-flex justify-content-around' style={{ marginTop: '20px' }}>
         <button className='btn btn-outline-primary' disabled={character === 1 ? true : false} onClick={Previus}>Anterior</button>
         <button className='btn btn-outline-primary' onClick={reset}>Ir al incio</button>
         <button className='btn btn-outline-primary' onClick={Next} style={{ right: '0px' }}>Siguiente</button>
