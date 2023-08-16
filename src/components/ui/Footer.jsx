ebefc1658c1b294b80169509b7ce9aed0730e7cd
Decxo360 <diegolundstedt99@gmail.com>
2023-08-15 21:03:23 -0400

    feat: cambio del color

--- src/components/ui/Footer.jsx
+++ src/components/ui/Footer.jsx
@@ -3,9 +3,9 @@ import React from 'react'
 export const Footer = () => {
     return (
         <div
-            className='mt-5' 
+            className='mt-5 bg-dark.bg-gradient text-white' 
             style={{
-                backgroundColor:'#7c7a76',
+                backgroundColor:'',
                 color: 'black',
                 borderTop: '1px solid wheat',
                 height:'230px'
@@ -15,8 +15,8 @@ export const Footer = () => {
             <div
                 className='d-flex flex-row mt-5 mb-0 ms-2 justify-content-between'>
                 <div className='d-flex flex-column'>
-                    <h6 style={{textAlign:'center'}}>Los endpoints trabajados son de: <a style={{color:'black'}} href={'https://rickandmortyapi.com/documentation/#character'}> Documentacion </a></h6>
-                    <h6 style={{textAlign:'center'}}>La informacion consumida en esta web fue directamene sacada de: <br /> <a style={{color:'black'}} href='https://rickandmortyapi.com/'> Rick and Morty Api</a></h6>
+                    <h6 style={{textAlign:'center'}}>Los endpoints trabajados son de: <a style={{color:'white'}} target='_blank' href={'https://rickandmortyapi.com/documentation/#character'}> Documentacion </a></h6>
+                    <h6 style={{textAlign:'center'}}>La informacion consumida en esta web fue directamene sacada de: <br /> <a style={{color:'white'}} target='_blank' href='https://rickandmortyapi.com/'> Rick and Morty Api</a></h6>
                 </div>
                 <div className='d-flex flex-column me-2'>
                     <h3 style={{ textAlign: 'center' }}>Informacion personal para oferta laboral</h3>
@@ -30,3 +30,5 @@ export const Footer = () => {
         </div>
     )
 }
+
+// web scraping
