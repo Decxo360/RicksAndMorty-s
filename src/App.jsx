836044a408c8ae34deea4d108b08287045a6226b
Decxo360 <diegolundstedt99@gmail.com>
2023-08-16 09:15:05 -0400

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -25,13 +25,6 @@ export const App = () => {
   return (
     <div>
       <img src='https://res.cloudinary.com/drp7cwb8s/image/upload/v1692131103/kisspng-rick-sanchez-youtube-morty-smith-rick-and-morty-5af4f912cb4576.6530769515260039868326_ijm2yk.png' className='img-fluid' alt='algoctm' style={{ width: '25%', marginLeft: '39%' }} />
-      <div className='d-flex justify-content-around' style={{ marginTop: '20px' }}>
-        <button className='btn btn-outline-primary' disabled={character === 1 ? true : false} onClick={Previus}>Anterior</button>
-        <button className='btn btn-outline-primary' onClick={reset}>Ir al incio</button>
-        <button className='btn btn-outline-primary' onClick={Next} style={{ right: '0px' }}>Siguiente</button>
-      </div>
-        
-
       <div className='d-flex flex-wrap'>
         {!isLoading 
         ? 
@@ -49,8 +42,11 @@ export const App = () => {
         : 
         <>Cargando.....</>}
       </div>
-
-
+      <div className='d-flex justify-content-around' style={{ marginTop: '20px' }}>
+        <button className='btn btn-outline-primary' disabled={character === 1 ? true : false} onClick={Previus}>Anterior</button>
+        <button className='btn btn-outline-primary' onClick={reset}>Ir al incio</button>
+        <button className='btn btn-outline-primary' onClick={Next} style={{ right: '0px' }}>Siguiente</button>
+      </div>
     </div>
   )
 }
