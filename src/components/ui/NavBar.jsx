ea3bc25f454c1c2d557987b7f43d546686ac9cf3
Decxo360 <diegolundstedt99@gmail.com>
2023-08-15 21:03:37 -0400

    feat: navbar centrado

--- src/components/ui/NavBar.jsx
+++ src/components/ui/NavBar.jsx
@@ -3,23 +3,25 @@ import { Link, NavLink } from 'react-router-dom'
 
 export const NavBar = () => {
     return (
-        <nav className="navbar navbar-expand-lg bg-light">
-            <div className="container-fluid">
-                <Link className='navbar-brand' to={'/'}>Rick And Morty</Link>
-                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                    <span className="navbar-toggler-icon"></span>
-                </button>
-                <div className="collapse navbar-collapse" id="navbarNav">
-                    <ul className="navbar-nav">
-                        <li className="nav-item">
-                            <NavLink className={({isActive})=>(isActive ? 'nav-link active' : 'nav-link' )}  to='/' >Home</NavLink>
-                        </li>
-                        <li className="nav-item">
-                            <NavLink className={({isActive})=>(isActive ? 'nav-link active' : 'nav-link' )}  to='/search' >Search</NavLink> 
-                        </li>
-                    </ul>
+        <div className='d-flex justify-content-center'>
+            <nav className="navbar navbar-expand-lg bg-dark.bg-gradient ">
+                <div className="container-fluid">
+                    <Link className='text-white navbar-brand' to={'/'}>Rick And Morty</Link>
+                    <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+                        <span className="navbar-toggler-icon"></span>
+                    </button>
+                    <div className="collapse navbar-collapse" id="navbarNav">
+                        <ul className="navbar-nav ">
+                            <li className="nav-item">
+                                <NavLink className={({ isActive }) => (isActive ? 'nav-link active text-warning' : 'nav-link text-white')} to='/' >Home</NavLink>
+                            </li>
+                            <li className="nav-item">
+                                <NavLink className={({ isActive }) => (isActive ? 'nav-link active text-warning' : 'nav-link text-white')} to='/search' >Search</NavLink>
+                            </li>
+                        </ul>
+                    </div>
                 </div>
-            </div>
-        </nav>
+            </nav>
+        </div>
     )
 }
