5d783348e843eff3585d7a684cea92be883acfb8
Decxo360 <diegolundstedt99@gmail.com>
2023-08-15 21:22:50 -0400

    feat: estilos

--- src/components/CharacterContent.jsx
+++ src/components/CharacterContent.jsx
@@ -10,27 +10,30 @@ export const CharacterContent = ({ img, name, status, species, gender, id }) => 
 
   return (
     <div
-        className="cursor" 
+      style={{
+        marginTop: '50px',
+        border: '1px solid #424242',
+        width: '300px',
+        borderRadius: '5px',
+        marginLeft: '135px',
+        order: '4'
+      }}>
+      <div
+        className="cursor d-flex justify-content-center"
         onClick={onGoToDetail}
-        style={{ 
-          marginTop: '20px', 
-          border: '1px solid #424242', 
-          width: '200px',
-          borderRadius: '5px',
-          marginLeft: '135px', 
-          order: '4' 
-          }}>
-      <div style={{ marginLeft: '22px', marginTop: '10px' }}>
-        <img src={img}
-          alt="" style={{ width: '150px', height: '150px', borderRadius: '75px' }} />
+      >
+        <div style={{ marginLeft: '22px', marginTop: '10px' }}>
+          <img src={img}
+            alt="" style={{ width: '150px', height: '150px', borderRadius: '75px' }} />
+        </div>
       </div>
-      <div style={{ marginTop: '20px' }}>
+      <div className="d-flex justify-content-center flex-column" style={{ marginTop: '20px', textAlign:"center"}}>
         <h5 style={{ color: 'wheat', marginLeft: '10px', marginRight: '10px' }}>Nombre: {name}</h5>
         <h5 style={{ color: 'wheat', marginLeft: '10px', marginRight: '10px' }}>Vivo o Muerto: {status}</h5>
         <h5 style={{ color: 'wheat', marginLeft: '10px', marginRight: '10px' }}>Raza: {species}</h5>
         <h5 style={{ color: 'wheat', marginLeft: '10px', marginRight: '10px' }}>Genero: {gender}</h5>
       </div>
-
     </div>
   )
 }
+
